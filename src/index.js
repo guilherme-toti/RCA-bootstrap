a4350c7e67f2a5300e2a2770aa9b0f53e8903c0d
Sandro Toti <sandrototi@MacBook-Pro-de-Sandro.local>
2016-08-19 17:11:14 -0300

    add redux and todo-list

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { browserHistory, Router, Route, IndexRoute } from 'react-router'
+import { browserHistory, Router, Route, IndexRoute } from 'react-router';
 
-import App from './components/App'
-import Home from './components/Home'
-import About from './components/About'
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import todoApp from './reducers'
+
+let store = createStore(todoApp, window.devToolsExtension && window.devToolsExtension());
+
+import App from './components/App';
+import Home from './components/Home';
+import About from './components/About';
 
 render(
-  <Router history={browserHistory}>
-    <Route path='/' component={App}>
-      <IndexRoute component={Home} />
-      <Route path='about' component={About} />
-    </Route>
-  </Router>,
+  <Provider store={store}>
+    <Router history={browserHistory}>
+      <Route path='/' component={App}>
+        <IndexRoute component={Home} />
+        <Route path='about' component={About} />
+      </Route>
+    </Router>
+  </Provider>,
   document.getElementById('root')
 )
